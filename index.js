915d6ae467256f78506ca4632bb0ff3d52856221
Chris DeHaan <christopherwdehaan@gmail.com>
2023-09-09 22:30:45 -0400

    put some initial code in with some questions

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Installation instructions?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the intended usage of this project?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license would you like to use?',
+            choices: ['MIT', 'APACHE-2.0'],
+            default: 'MIT'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Any contributors?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What is the title of your project?'
+        },
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        
+    })
+ }
 
 // Function call to initialize app
 init();
